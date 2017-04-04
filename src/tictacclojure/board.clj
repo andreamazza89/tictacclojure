913f328b347ad9f9ae580d302747121857c9eabc
Andrea Mazzarella <andrea.mazzarella89@gmail.com>
2017-04-04 11:08:27 +0100

    First implementation of board

--- src/tictacclojure/board.clj
+++ src/tictacclojure/board.clj
@@ -1,3 +1,19 @@
 (ns tictacclojure.board)
 
-(defn smoke [] true)
+(defn new-board
+  []
+  [0 1 2 3 4 5 6 7 8])
+
+(defn empty?
+  [board]
+  (every? integer? board))
+
+(defn add-move
+  [board [position mark]]
+  (map
+    (fn [cell] (if (= cell position) mark cell))
+    board))
+
+(defn moves-available
+  [board-with-move]
+  (filter integer? board-with-move))
--- test/tictacclojure/board_test.clj
+++ test/tictacclojure/board_test.clj
@@ -4,4 +4,19 @@
 
 (deftest board-test
   (testing "New board"
-    (is (= (board/smoke) true) "shmoke")))
+    (let [new-board (board/new-board)
+          board-with-move (board/add-move new-board [0 :x])]
+      (is (= (board/empty? new-board) true) "A new board is empty")
+      (is (= (board/empty? board-with-move) false) "A board with a move is not empty")))
+
+   (testing "Moves available"
+     (let [board-one [:x 1 2 3 4 5 6 7 8]
+           board-two [:x :o 2 3 4 5 6 7 8]]
+       (is (= [1 2 3 4 5 6 7 8]
+              (board/moves-available board-one)) "Only available moves are reported (example one)")
+       (is (= [2 3 4 5 6 7 8]
+              (board/moves-available board-two)) "Only available moves are reported (example two)")))
+
+   (testing "Making a move"
+     (is (= [:x] (board/add-move [0] [0 :x])) "Adds the given mark to the given position (example one)")
+     (is (= [0 :o] (board/add-move [0 1] [1 :o])) "Adds the given mark to the given position (example two)")))
