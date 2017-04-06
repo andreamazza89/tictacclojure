ae9846307d57418abb2a8132b7c1ddd1d02234fc
Andrea Mazzarella <andrea.mazzarella89@gmail.com>
2017-04-06 14:53:28 +0100

    Move initial greeting into entrypoint (main function)

--- src/tictacclojure/core.clj
+++ src/tictacclojure/core.clj
@@ -1,8 +1,14 @@
 (ns tictacclojure.core
   (:require [tictacclojure.game :as game]
             [tictacclojure.board :as board]
+            [tictacclojure.text-prompts :as text]
             [tictacclojure.console-runner :as runner]))
+
+(defn- console-out
+  [message]
+  (println message))
 
 (defn -main
   []
+  (console-out text/greeting)
   (runner/play-game (game/create-game (board/new-board) [:x :human] [:o :easy-ai])))
